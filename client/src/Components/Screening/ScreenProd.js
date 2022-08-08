e2248029e2e471aea086dd5c82eee64476e73d08
Mihir Harshe <mihir.harshe12@gmail.com>
2022-08-09 00:42:44 +0530

    added batch count logic along with ToggleButtons for batches

--- client/src/Components/Screening/ScreenProd.js
+++ client/src/Components/Screening/ScreenProd.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Container from '@mui/system/Container';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import { DataGrid } from '@mui/x-data-grid';
 import { useParams } from 'react-router-dom';
 
@@ -11,6 +15,11 @@ export const ScreenProd = () => {
 
     const [selectedProduct, setSelectedProduct] = useState([]);
     const [prodBoq, setProdBoq] = useState([]);
+    const [selectedBatch, setSelectedBatch] = useState(0);
+
+    const handleSelectBatch = (e, batchNumber) => {
+        setSelectedBatch(batchNumber);
+    }
 
     useEffect(() => {
         const fetchProd = async () => {
@@ -28,7 +37,7 @@ export const ScreenProd = () => {
     console.log(selectedProduct);
 
     useEffect(() => {
-        if(selectedProduct.name) {
+        if (selectedProduct.name) {
             const fetchBoq = async () => {
                 try {
                     const res = await axios.get(`http://localhost:5000/boq/name/${selectedProduct.name}`);
@@ -44,26 +53,39 @@ export const ScreenProd = () => {
 
     console.log(prodBoq);
 
+    const batch_size = prodBoq.batch_size;
+    const prodQty = selectedProduct.qty;
+
+    const batchCount = Math.ceil(prodQty / batch_size);
+
+    console.log(prodQty, batch_size, batchCount);
 
 
     return (
         <>
             <Container maxWidth='lg' sx={{ marginTop: 2 }}>
                 <Box display='flex' alignItems='center' justifyContent='space-between' marginBottom={1}>
-                    <Box component='span'>SCREENING : {selectedProduct.name}</Box>
+                    <Box component='span'>PRODUCT : {selectedProduct.name}</Box>
                 </Box>
-                <Box style={{ display: 'flex', height: '100%', width: '100%', backgroundColor: 'white' }}>
-                    {/* <Box style={{ flexGrow: 1 }}>
-                        <DataGrid
-                            autoHeight
-                            getRowId={(prod) => prod._id}
-                            rows={productions}
-                            columns={columns}
-                            pageSize={pageSize}
-                            onPageSizeChange={(pageSize) => setPageSize(pageSize)}
-                            rowsPerPageOptions={[5, 10, 20, 50, 100]}
-                        />
-                    </Box> */}
+                Choose your batch :
+                <Box style={{ display: 'flex', height: '100%', width: '100%'}}>
+                    {/* {batchCount ?
+                        [...Array(batchCount)].map((_, i) => (
+                            <Button variant="contained" key={i} sx={{ marginRight: 2 }}>batch {i + 1}</Button>
+                        ))
+                    : <CircularProgress />} */}
+                    <ToggleButtonGroup 
+                        value={selectedBatch} 
+                        onChange={handleSelectBatch}
+                        exclusive
+                        sx={{backgroundColor: 'white'}}
+                    >
+                        {batchCount ?
+                            [...Array(batchCount)].map((_, i) => (
+                                <ToggleButton key={i} value={i + 1}>batch {i + 1}</ToggleButton>
+                            ))
+                        : null}
+                    </ToggleButtonGroup>
                 </Box>
             </Container>
         </>
